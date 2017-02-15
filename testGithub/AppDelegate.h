6ffe26bb16cbd5b4716cfe420078e1f4d979262b
lixue <464159169@qq.com>
2017-02-15 15:53:05 +0800

    Initial Commit

---
+++ testGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGithub
+//
+//  Created by Yuki on 17/2/15.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGithub
+//
+//  Created by Yuki on 17/2/15.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
